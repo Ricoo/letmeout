c7c517e67a56b8647b9575120dc50c17c0846e73
Eric Hegedus <hegedus.e77@gmail.com>
2020-08-22 00:10:17 +0200

    structural update

---
+++ Assets/Scripts/AlphaController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaController : MonoBehaviour
+{
+    public GameObject player;
+    private float alpha = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < player.transform.position.y) {
+            Color tmp = GetComponent<SpriteRenderer>().color;
+            alpha -= 1f*Time.deltaTime;
+            alpha = alpha < .2f ? .2f : alpha;
+            tmp.a = alpha;
+            GetComponent<SpriteRenderer>().color = tmp;
+        }
+        else if (alpha < 1f ){
+            Color tmp = GetComponent<SpriteRenderer>().color;
+            alpha += 1f*Time.deltaTime;
+            alpha = alpha > 1f ? 1f : alpha;
+            tmp.a = alpha;
+            GetComponent<SpriteRenderer>().color = tmp;    
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Time.deltaTime * Input.GetAxis("Horizontal");
+        float vert = Time.deltaTime * Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(moveSpeed * horiz, moveSpeed * vert, 0);
+    }
+}
