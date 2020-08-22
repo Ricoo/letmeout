2b62c3fa2ed51c41bec2a5074d38e26fb63a1a8a
Eric Hegedus <hegedus.e77@gmail.com>
2020-08-22 11:11:02 +0200

    added colliders to walls/players

--- Assets/Scripts/AlphaController.cs
+++ Assets/Scripts/AlphaController.cs
@@ -10,7 +10,9 @@ public class AlphaController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (player == null) {
+            player = GameObject.Find("/Player");
+        }
     }
 
     // Update is called once per frame
