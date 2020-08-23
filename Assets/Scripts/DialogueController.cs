921df4996879d7ea8dcb8d7060c69aff2e676232
Eric Hegedus <hegedus.e77@gmail.com>
2020-08-23 17:28:08 +0200

    updated interaction system

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+
+public class DialogueController : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Intervention {
+        public Sprite portrait;
+         [TextArea(3,10)]
+        public string line;
+    }
+
+    [System.Serializable]
+    public class Dialogue {
+        public string id;
+        [SerializeField]
+        public List<Intervention> interventions = new List<Intervention>();
+    }
+
+    public List<Dialogue> dialogues = new List<Dialogue>();
+    // Start is called before the first frame update
+
+    public GameObject textBox;
+    public GameObject portrait;
+    public GameObject frame;
+    private Dialogue runningDialogue;
+    private int currentLine;
+    private float elapsedTime;
+    private Action callback = null;
+    void Start()
+    {
+        runningDialogue = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (Input.GetKey("space") && runningDialogue != null) {
+            nextLine();
+        }
+        
+    }
+
+    public void runDialogue(string id, Action cb) {
+        foreach (Dialogue d in dialogues) {
+            if (d.id == id) {
+                callback = cb;
+                runningDialogue = d;
+                currentLine = 0;
+                frame.SetActive(true);
+                printLine();
+                return ;
+            }
+        }
+        throw(new KeyNotFoundException("Dialogue not found !")) ; 
+    }
+
+    public void nextLine() {
+        if (elapsedTime < 1) {
+            return;
+        }
+        currentLine += 1;
+
+        if (currentLine == runningDialogue.interventions.Count) {
+            runningDialogue = null;
+            callback();
+            frame.SetActive(false);
+            return;
+        }
+        printLine();
+    }
+
+    public void printLine() {
+        textBox.GetComponent<Text>().text = runningDialogue.interventions[currentLine].line;
+        portrait.GetComponent<SpriteRenderer>().sprite = runningDialogue.interventions[currentLine].portrait;
+        portrait.transform.GetComponent<SpriteRenderer>().material.color = setAlpha(portrait.transform.GetComponent<SpriteRenderer>().material.color, 0);
+        StartCoroutine(FadeTo(portrait, 1.0f, 1.0f));
+        elapsedTime = 0;
+    }
+
+    public Dialogue getRunningDialogue() {
+        return this.runningDialogue;
+    }
+
+    public Color setAlpha(Color c, float alpha) {
+        return new Color(c.r, c.g, c.b, c.a);
+    }
+    IEnumerator FadeTo(GameObject go, float aValue, float aTime) {
+      float alpha = go.transform.GetComponent<SpriteRenderer>().material.color.a;
+      for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+      {
+          Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha,aValue,t));
+          go.transform.GetComponent<SpriteRenderer>().material.color = newColor;
+          yield return null;
+      }
+    }
+}
---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    /** Type is the type of the interaction :
+        - Dialogue is a simple dialogue with one or more characters
+        - Pickup adds an item to the inventory
+        - Requires checks if the player has a required item, and outputs one
+          of two subsequent interactions
+        - GameObjectToggle toggles a GameObject to be hidden or displayed
+        - UpdateSprite updates the sprite of the interactable item
+    */
+    public enum Type {
+        Dialogue,
+        Pickup,
+        Requires,
+        GameObjectToggle,
+        UpdateSprite
+    }
+
+    [SerializeField]
+    public Type t;
+    public string dialogueId;
+    public GameObject go;
+    public GameObject nok;
+    public Sprite altSprite;
+    public string item;
+
+
+    public bool used = false;
+    public bool repeat = false;
+    private GameObject player;
+    public GameObject parent;
+    private PlayerController pc;
+    private DialogueController dc;
+    private InventoryController ic;
+    public GameObject target;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        pc = player.GetComponent<PlayerController>();
+        dc = GameObject.Find("DialogueObject").GetComponent<DialogueController>();
+        ic = GameObject.Find("Inventory").GetComponent<InventoryController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void interact(GameObject target) {
+        // Depending on the interaction type, the actions to apply are different
+        if (used) {
+            pc.interacting = false;
+            return;
+        }
+        pc.interacting = true;
+        if (this.target == null) {
+            this.target = target;
+        } else {
+            target = this.target;
+        }
+        switch (t) {
+            case Type.Dialogue:
+                dc.runDialogue(dialogueId, ()=>{
+                    this.markAsDone();});
+                break;
+            case Type.Pickup:
+                ic.addItem(item);
+                target.SetActive(false);
+                markAsDone();
+                break;
+            case Type.Requires:
+                if (ic.hasItem(item, true)) {
+                    markAsDone(go);
+                } else {
+                    markAsDone(nok);
+                }
+                break;
+            case Type.GameObjectToggle:
+                go.SetActive(!go.activeSelf);
+                markAsDone();
+                break;
+            case Type.UpdateSprite:
+                target.GetComponent<SpriteRenderer>().sprite = altSprite;
+                markAsDone();
+                break;
+        }
+    }
+
+    // marks the interaction as accomplished, except if the interaction is on repeat
+    // then executes the next interaction if there is one available
+    public void markAsDone(GameObject next=null) {
+        used = true;
+        if(next == null) {
+            Interaction[] list = GetComponentsInChildren<Interaction>();
+            if (list.Length > 1) {
+                list[1].interact(target);
+            } else {
+                player.GetComponent<PlayerController>().interacting = false;
+            }
+        } else {
+            next.GetComponent<Interaction>().interact(target);
+        }
+        if (repeat) {
+            used = false;
+        } 
+
+    }
+}
